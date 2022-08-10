31616192901eb9cc07982da7179016366a116d75
PaulaDuduta <100792374+PaulaDuduta@users.noreply.github.com>
2022-08-10 21:58:33 +0300

    S06

--- M01S06/ex04/app.js
+++ M01S06/ex04/app.js
@@ -5,19 +5,34 @@
     const stage = document.querySelector('.stage');
 
     stage.addEventListener('mouseover', function () {
+      const message = 'Mouseul este pe scena';
       console.log('Mouseul este pe scena');
-      const p = document.createElement('p');
-      p.innerText = 'Mouseul este pe scena';
+      // const p = document.createElement('p');
+      // p.innerText = 'Mouseul este pe scena';
 
-      document.body.append(p);
+      // document.body.append(p);
+      showMessage(message);
     });
 
     stage.addEventListener('mouseout', function () {
+      const message = 'Mouseul NU este pe scena';
       console.log('Mouseul NU este pe scena.');
-      const p = document.createElement('p');
-      p.innerText = 'Mouseul NU este pe scena';
 
-      document.body.append(p);
+      // const p = document.createElement('p');
+      // p.innerText = 'Mouseul NU este pe scena';
+
+      // document.body.append(p);
+      showMessage(message);
     });
   });
+
+  //hoisting
+
+  function showMessage(message) {
+    const paragraphElement = document.createElement('p');
+    paragraphElement.innerText = message;
+    paragraphElement.classList.add('message');
+
+    document.body.append(paragraphElement);
+  }
 })();
