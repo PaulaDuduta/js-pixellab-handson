348eb83ba6ea47f18e75ca643ba9f0d3e9874f6e
PaulaDuduta <100792374+PaulaDuduta@users.noreply.github.com>
2022-08-17 22:01:00 +0300

    JS-workshop & JQuery

--- M02S02/ex01/app.js
+++ M02S02/ex01/app.js
@@ -4,3 +4,33 @@
 // p.classList.add('clase', 'de', 'css');
 
 // document.body.appendChild(p);
+
+const $p = $('<p>', {
+  text: 'Textul care trebuie sa apara in paragraf',
+  id: 'myParagraph',
+  class: 'clase de css',
+});
+
+const $msg = $('<p>', {
+  id: 'message',
+  text: 'Parola nu trebuie sa fie mai mica de 5 caractere',
+});
+
+// $(document.body).append($p);
+const $container = $('.container'); //also good $body and 'body'
+$p.appendTo($container);
+
+$container.append($msg);
+
+setTimeout(function () {
+  $msg.addClass('error');
+}, 5000);
+
+$('<span>', {
+  id: 'example',
+  text: 'ParolaMea',
+}).appendTo($msg);
+
+$('<h2>', {
+  text: 'Mesaje',
+}).prependTo($container);
